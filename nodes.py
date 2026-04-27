7ef36d646e6126e69611b5399af9fe9629441045
khairul-islam99 <bdkhairul402@gmail.com>
2026-04-27 16:44:46 +0600

    created nodes.py

---
+++ nodes.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import ToolNode
+from state import AgentState
+from tools import search_available_properties, get_listing_details, create_booking
+
+load_dotenv()
+
+# Setup LLM using OpenRouter and Nvidia Nemotron model
+llm = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    model="nvidia/nemotron-3-nano-30b-a3b:free",
+)
